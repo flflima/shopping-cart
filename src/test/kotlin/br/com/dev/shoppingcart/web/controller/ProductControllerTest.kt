bdff0d586cc3848efd10cfda7456ee915d21e42f
Felipe Lima <flf.felipe@gmail.com>
2021-03-30 17:29:50 -0300

    test: adiciona teste de contrato para consulta de produto

--- src/test/kotlin/br/com/dev/shoppingcart/web/controller/ProductControllerTest.kt
+++ src/test/kotlin/br/com/dev/shoppingcart/web/controller/ProductControllerTest.kt
@@ -1,10 +1,11 @@
 package br.com.dev.shoppingcart.web.controller
 
 import br.com.dev.shoppingcart.mocks.ProductMock
+import io.javalin.http.NotFoundResponse
 import io.mockk.every
 import io.mockk.junit5.MockKExtension
 import io.restassured.RestAssured
-import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
@@ -21,9 +22,22 @@ class ProductControllerTest : BaseTest() {
             .then()
             .assertThat()
             .statusCode(200)
-            .body("name", CoreMatchers.equalTo("Camiseta"))
-            .body("price", CoreMatchers.equalTo(100.0f))
-            .body("description", CoreMatchers.equalTo(""))
-            .body("category", CoreMatchers.equalTo("Vestuário"))
+            .body("name", equalTo("Camiseta"))
+            .body("price", equalTo(100.0f))
+            .body("description", equalTo(""))
+            .body("category", equalTo("Vestuário"))
+    }
+
+    @Test
+    fun `given a request for an non existing product must return a not found status code`() {
+        every { productService.getProductById(any()) } throws NotFoundResponse()
+
+        RestAssured.given()
+            .`when`()
+            .get("product/1")
+            .then()
+            .assertThat()
+            .statusCode(404)
+            .body(equalTo("Not found"))
     }
 }
